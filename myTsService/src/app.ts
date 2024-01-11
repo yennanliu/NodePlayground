097b3ddfa3cf00334ece39a163fe391212da2fc9
yennanliu <f339339@gmail.com>
2024-01-11 17:20:35 +0800

    update

--- myTsService/src/app.ts
+++ myTsService/src/app.ts
@@ -2,9 +2,29 @@ import express from 'express';
 
 const app = express();
 const port = 3000;
+// var server = app.listen(port, function (){
+//
+//     if (port === 3000) {
+//         console.log('true')
+//     }
+//     console.log(`server is listening on ${port} !!!`);
+// })
+
+// router
 app.get('/', (req, res) => {
-    res.send('My server is working !!!!!');
+    res.send('---> My server is working !!!!!');
 });
+
+
+app.get('/test1', (req, res) => {
+    res.send('---> test1 !!!');
+});
+
+app.post('/', (req, res) => {
+    console.log("main page POST")
+    res.send('hello POST');
+});
+
 
 
 app.listen(port, () => {
